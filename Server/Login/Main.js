89beb9ee523dafa9c64942e1ca8357d62379e0c1
domi <t01048187045@gmail.com>
2023-05-27 13:02:03 +0900

    음

--- Server/Login/Main.js
+++ Server/Login/Main.js
@@ -8,4 +8,10 @@ app.use(express.json());
 // 로그인 로직
 app.post("/login", require("./Login.js"));
 
+// 이름 중복확인
+app.get("/nameCompare/:name", function(req, res) {
+    const name = req.params.name;
+    console.log(name);
+});
+
 app.listen(Config.LoginPort, () => console.info("로그인 시스템이 준비되었습니다. Port: "+ Config.LoginPort));

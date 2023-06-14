6b95e5f1e4252aaefc8745438157fae31cddc493
domi <t01048187045@gmail.com>
2023-06-14 23:38:58 +0900

    벅으버그버그

--- Server/TCP/MakeRobot/main.js
+++ Server/TCP/MakeRobot/main.js
@@ -6,11 +6,12 @@ TriggerEvent["MakeRobot.GetSO"] = function(id) {
     if (Player === undefined || !Player.ready) return;
 
     const preset = Player.preset;
-    for (const key in preset) {
-        const element = preset[key];
-        preset[key] = element || "";
-    }
-
     Player.socket.send("MakeRobot.ResultSO", equipmentUtils.GetAllItem(id));
-    Player.socket.send("MakeRobot.SendPreset", preset);
+    Player.socket.send("MakeRobot.SendPreset", {
+        left: preset.left || "",
+        right: preset.right || "",
+        head: preset.head || "",
+        body: preset.body || "",
+        leg: preset.leg || ""
+    });
 }

48ad538f775caf1c15519f813dde882c4a4d7245
VsPot07 <cocoacake84@gmail.com>
2023-07-09 21:30:38 +0900

    0709

--- Assets/MAESTRO/Scripts/FastAttackUI.cs
+++ Assets/MAESTRO/Scripts/FastAttackUI.cs
@@ -16,16 +16,39 @@ public class FastAttackUI : MonoBehaviour
 
     Dictionary<Button, StageEnterData> _stageEnterBtn = new Dictionary<Button, StageEnterData>();
 
+    VisualElement _panel;
+    Button _okBtn;
+    bool onPanel;
+    bool isRope;
+
+    Label _ropeCountTxt;
+    Label _errorTxt;
+
+    [field:SerializeField] public int RopeCount { get; set ; }
 
     private void Awake()
     {
+        RopeCount = 9;
         _doc = GetComponent<UIDocument>();
     }
 
     private void DownEnterCount(StageEnterData data)
     {
-        data.LeftCount--;
-        data.CountTxt.text = $"{data.LeftCount}/3";
+        if(data.LeftCount > 0 && RopeCount > 0)
+        {
+            data.LeftCount--;
+            RopeCount--;
+            data.CountTxt.text = $"{data.LeftCount}/3";
+            _ropeCountTxt.text = $"{RopeCount} / 9";
+        }
+        else
+        {
+            if(RopeCount == 0)
+            {
+                isRope = true;
+            }
+            PanelSet(isRope);
+        }
     }
 
     private void OnEnable()
@@ -45,5 +68,28 @@ public class FastAttackUI : MonoBehaviour
             item.Key.clicked += () => DownEnterCount(item.Value);
         }
 
+        _panel = _root.Q<VisualElement>("Panel");
+        _okBtn = _root.Q<Button>("OkBtn");
+        _ropeCountTxt = _root.Q<Label>("RopeCount");
+        _okBtn.clicked += () => PanelSet(false);
+        _errorTxt = _root.Q<Label>("ErrorTxt");
+    }
+
+    private void PanelSet(bool isRope)
+    {
+        if (!isRope)
+            _errorTxt.text = "금일 도전 횟수를 전부\n 사용하셨습니다.";
+        else
+            _errorTxt.text = "금일 지급된 횟수를 전부\n 사용하셨습니다.";
+
+        if(onPanel)
+        {
+            _panel.RemoveFromClassList("on");
+        }
+        else
+        {
+            _panel.AddToClassList("on");
+        }
+        onPanel = !onPanel;
     }
 }

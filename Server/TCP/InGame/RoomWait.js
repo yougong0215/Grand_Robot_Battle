f5d60e996cffcf1e141614ba06c9caa7c8cf407a
domi <domi@domi.kr>
2023-09-13 09:19:29 +0900

    AIAIAIAIAI

--- Server/TCP/InGame/RoomWait.js
+++ Server/TCP/InGame/RoomWait.js
@@ -11,9 +11,14 @@ RoomManager.RoomClass.prototype.WaitForPlayer = function() {
     });
 }
 
-TriggerEvent["ingame.ready"] = function(id) {
+TriggerEvent["ingame.ready"] = function(id, ai) {
     const player = UserList[id];
     if (player === undefined) return;
+
+    if (ai) {
+        GetPartsWithAI(id);
+        return;
+    }
     
     const room = RoomManager.getRoomToPlayer(id);
     if (room === undefined || room.waitData === undefined || room.waitData[id] !== false) return;
@@ -29,4 +34,31 @@ TriggerEvent["ingame.ready"] = function(id) {
     // 모두다 준비 됨
     delete room.waitData;
     room.GameStart();
+}
+
+const equipmentUtils = require("../lib/equipmentUtils.js");
+const itemStatManager = require("../ItemStat");
+function GetPartsWithAI(id) {
+    const player = UserList[id];
+    if (player === undefined) return;
+
+    const playerWear = {};
+    for (const [part, itemToken] of Object.entries(player.preset)) {
+        if (itemToken === undefined) continue;
+        const item = player.inventory.equipment[itemToken];
+        if (item === undefined) continue;
+
+        const itemStat = itemStatManager.itemStats[Number(item.level)][equipmentUtils.GetGradID(item.grade) + "_" + item.code];
+        if (itemStat === undefined) continue;
+
+        playerWear[part] = {
+            id: item.code,
+            ...itemStat
+        }
+    }
+
+    player.socket.send("ingame.AIinit", {
+        name: player.name,
+        ...playerWear
+    });
 }

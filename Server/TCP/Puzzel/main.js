2b54530a5b51cdb8a88d7e1165d215a80e48e4d1
domi <domi@domi.kr>
2023-09-13 20:06:42 +0900

    인벤토리 서버 연동

--- Server/TCP/Puzzel/main.js
+++ Server/TCP/Puzzel/main.js
@@ -44,12 +44,14 @@ TriggerEvent["puzzel.createParts"] = function(id, data) {
     if (!itemUtil.Try(id, data.part+"_puzzel", TryValue)) {
         // 업글할 퍼즐이 부족합니다 ㄱㄱ
         console.log(`[puzzel] ${player.name}(${id})님이 파츠 교환에 실패했어요.`);
+        player.socket.send("puzzel.errorwindow", "개수가 부족하여 파츠를 교환 할 수 없습니다.");
         return;
     }
 
     // 파츠 줘
-    equipmentUtil.AddItem(id, data.part, 0, data.grade);
+    equipmentUtil.AddItem(id, data.part, 1, data.grade);
     console.log(`[puzzel] ${player.name}(${id})님이 파츠를 교환하였어요. ${data.part} / ${data.grade}등금`);
+    player.socket.send("puzzel.errorwindow", "domi.eventsuccess:"+data.part+":"+data.grade);
 }
 
 // 끝에 _puzzel 이 있는지 확인함

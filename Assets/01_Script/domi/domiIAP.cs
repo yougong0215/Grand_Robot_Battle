80fcb9171f05c4730908d8393f9bd6fe628cb15a
domi <domi@domi.kr>
2023-09-18 18:13:30 +0900

    결제 완료

---
+++ Assets/01_Script/domi/domiIAP.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Unity.Services.Core;
+using UnityEngine.Purchasing;
+
+struct ShopPacket {
+    public string payload;
+    public string id;
+}
+
+public class domiIAP : MonoBehaviour, IStoreListener
+{
+    IStoreController m_StoreContoller;
+    Product cacheProduct;
+    UnityAction<bool> cacheCallback;
+
+    List<string> products = new();
+
+    public void AddProduct(string id) => products.Add(id);
+
+    public void ShowProduct(string id, UnityAction<bool> cb) {
+        cacheCallback = cb;
+        m_StoreContoller.InitiatePurchase(id);
+    }
+
+    void IAP_Complete(LitJson.JsonData data) {
+        if (!(bool)data) {
+            Debug.LogWarning("[domiIAP] Server Check Worng");
+            cacheCallback.Invoke(false);
+            return;
+        }
+
+        print("[domiIAP] Finish Pay");
+        cacheCallback.Invoke(true);
+    }
+
+    async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+        foreach (var item in products)
+            builder.AddProduct(item, ProductType.Consumable);
+
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+    private void OnDestroy() {
+        NetworkCore.EventListener.Remove("store.complete");
+    }
+
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        print("[domiIAP] Init Success");
+        NetworkCore.EventListener["store.complete"] = IAP_Complete;
+        m_StoreContoller = controller;
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        Debug.LogError("[domiIAP] Init Fail");
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error, string message)
+    {
+        Debug.LogError("[domiIAP] Init Fail / "+ message);
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+        cacheCallback?.Invoke(false);
+        cacheCallback = null;
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs purchaseEvent)
+    {
+        var product = cacheProduct = purchaseEvent.purchasedProduct;
+        var data = LitJson.JsonMapper.ToObject(product.receipt);
+
+        print("[domiIAP] Purchase Check Server / "+ product.definition.id);
+        NetworkCore.Send("store.buy", new ShopPacket() { payload = (string)data["Payload"], id = product.definition.id });
+
+        return PurchaseProcessingResult.Pending;
+    }
+}
--- Server/TCP/Store/main.js
+++ Server/TCP/Store/main.js
@@ -1,20 +1,22 @@
+const crystalUtil = require("../lib/crystalUtils.js");
 const iap = require("iap");
 const iap_token = require(__rootdir+"/Config/IAP_account.json");
 
-TriggerEvent["store.test"] = function(id, data) {
+TriggerEvent["store.buy"] = function(id, data) {
     const player = UserList[id];
     if (player === undefined) return;
     
     let purchaseToken;
     try {
-        purchaseToken = JSON.parse(JSON.parse(data).json).purchaseToken;
+        purchaseToken = JSON.parse(JSON.parse(data.payload).json).purchaseToken;
     } catch (err) {
+        // console.log("store.buy data : "+data);
         return;
     }
 
     iap.verifyPayment("google", {
         receipt: purchaseToken,
-        productId: "domi_test2",
+        productId: data.id,
         packageName: "com.Isthisno.sdfd",
         subscription: false,
         keyObject: iap_token
@@ -24,9 +26,34 @@ TriggerEvent["store.test"] = function(id, data) {
             return;
         }
 
-        console.log(err, result);
-        player.socket.send("store.complete", true);
-    });
+        if (!player.socket.isConnect()) {
+            console.log("결제 중 오프라인 : "+ result.productId + " / "+ id);
+            return;
+        }
 
-    
+        player.socket.send("store.complete", true);
+        PlayerBuyHandler(id, result.productId);
+    });
+}
+
+const GiveCrystal_List = {
+    "crystal_990": 90,
+    "crystal_4900": 520,
+    "crystal_9900": 990,
+    "crystal_19000": 2000,
+    "crystal_29000": 3000,
+    "crystal_49000": 5000,
+}
+function PlayerBuyHandler(id, productId) {
+    const player = UserList[id];
+    if (player === undefined) return;
+
+    const value = GiveCrystal_List[productId];
+    if (value === undefined) {
+        console.log(`${id}이가 ${productId}를 구매했지만 설정된게 없음`);
+        return;
+    }
+
+    crystalUtil.Add(id, value);
+    player.socket.send("Lobby.Reload", null);
 }

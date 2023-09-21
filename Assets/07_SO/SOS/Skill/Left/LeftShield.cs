8fa1bdad7e2708d69d90f4844bc0b4bcbab07333
yougong0215 <yougong0215@gmail.com>
2023-09-21 19:51:11 +0900

    AAAA

--- Assets/07_SO/SOS/Skill/Left/LeftShield.cs
+++ Assets/07_SO/SOS/Skill/Left/LeftShield.cs
@@ -12,8 +12,11 @@ public class LeftShield : PartSkillSO
 
     protected override IEnumerator UseingSKill()
     {
-        _pvp.SetText("체력이 100 증가했다.");
+        _pvp.SetText("체력이 200 증가했다.");
+        
         yield return new WaitForSeconds(0.8f);
+        
+        _pvp.SetHPValue(_me, -200);
         isEnd = true;
         //throw new System.NotImplementedException();
     }
